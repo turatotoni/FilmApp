492c58bc730bed5048c8d526ceda5cb6f0cf0e3e
turatotoni <turatotoni54@gmail.com>
2025-06-13 01:47:48 +0200

    Implemented user follow logic.

---
+++ app/src/main/java/com/example/filmapp/User.kt
@@ -0,0 +1,14 @@
+package com.example.filmapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val id: String,
+    val username: String,
+    val email: String,
+    val avatarID: Int, //prebacio sam string u int jer sam tako prvo napravio s avatarpicker i avataradapter; bio je veliki problem jer nisam mogo ucitat sliku jer sam loado kao string a ne kao id
+    val followerCount: Long = 0,
+    val followingCount: Long = 0
+) : Parcelable
